148f02b581454621ededf0f2929c383085c35d4f
AlexPrincipal <chrisjaog@gmail.com>
2025-05-28 23:14:20 -0700

    a

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
-app.use(bodyParser.json());
+app.use(bodyParser.json({ limit: '5mb' }));
+app.use(bodyParser.urlencoded({ extended: true, limit: '5mb' }));
 
 // Conexión a MongoDB
 conectarDB()
